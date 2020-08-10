414cca07a09c277332da0de9c455f1f9c4fa1710
Pebanh <peter.banh95@gmail.com>
2020-08-10 12:50:34 -0400

    Test for Phaser.js

---
+++ public/javascript/main.js
@@ -0,0 +1,46 @@
+var config = {
+  width: 800,
+  height: 600,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2]
+}
+
+
+var game = new Phaser.Game(config);
+
+// var config = {
+//   type: Phaser.AUTO,
+//   width: 800,
+//   height: 600,
+//   physics: {
+//     default: 'arcade',
+//     arcade: {
+//       gravity: { y: 200 }
+//     }
+//   },
+//   scene: {
+//     preload: preload,
+//     create: create
+//   }
+// };
+
+// var game = new Phaser.Game(config);
+
+// function preload() {
+//   this.load.setBaseURL('http://labs.phaser.io');
+
+//   this.load.image('sky', 'assets/skies/space3.png');
+//   this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+// }
+
+// function create() {
+//   this.add.image(400, 300, 'sky');
+
+//   var logo = this.physics.add.image(400, 100, 'logo');
+
+//   logo.setVelocity(100, 200);
+//   logo.setBounce(1, 1);
+//   logo.setCollideWorldBounds(true);
+
+//   emitter.startFollow(logo);
+// }
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const morgan = require('morgan')
 const express = require('express')
+var path = require("path");
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(morgan('dev'))
 
 app.get('/', function (req, res, next) {
-  res.send('Hello World')
+  res.sendFile(__dirname + '/index.html');
 })
 
 const PORT = process.env.PORT || 3000
